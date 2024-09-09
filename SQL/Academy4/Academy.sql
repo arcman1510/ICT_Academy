4ef8438090ecc633e73bfc953d44f2a5e00949b2
Nio4124 <antoniozottola41204@gmail.com>
2024-09-09 19:27:25 +0200

    E6

--- SQL/Academy4/Academy.sql
+++ SQL/Academy4/Academy.sql
@@ -39,3 +39,30 @@ where fine < CURRENT_DATE
 
 -- 6. Quali sono i nomi di tutti i Progetti ordinati 
 -- in ordine crescente di data di inizio?
+
+select id, nome, inizio
+from Progetto
+where fine < CURRENT_DATE
+order by inizio asc
+
+--7. Quali sono i nomi dei WP ordinati 
+--in ordine crescente (per nome)?
+
+
+
+--8. Quali sono (distinte) le cause di assenza
+--di tutti gli strutturati?
+
+
+
+--9. Quali sono (distinte) le tipologie 
+--di attività di progetto di tutti gli strutturati?
+
+
+
+--10. Quali sono i giorni distinti nei quali 
+--del personale ha effettuato attività 
+--non progettuali di tipo ‘Didattica’ ? 
+--Dare il risultato in ordine crescente
+
+
