aa004c6cec93b7f3a175c46610cc33f76fe99ff9
Nio4124 <antoniozottola41204@gmail.com>
2024-09-10 18:35:23 +0200

    10 settembre

---
+++ Python5/ServerWebFS/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@api.route('/', methods=['GET'])
+def index2():
+    return render_template('index2.html')
+
+api.run(host="127.0.0.1", port=8085)
