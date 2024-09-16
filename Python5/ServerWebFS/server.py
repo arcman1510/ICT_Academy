db4a748a7122057c201704887150b3725e894aa2
Nio4124 <antoniozottola41204@gmail.com>
2024-09-16 09:47:23 +0200

    Cielo1 inizio

--- Python5/ServerWebFS/server.py
+++ Python5/ServerWebFS/server.py
@@ -6,8 +6,17 @@ api = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@api.route('/', methods=['GET'])
-def index2():
-    return render_template('index2.html')
+@api.route('/regok', methods=['GET'])
+def regok():
+    return render_template('reg_ok.html')
+
+@api.route('/regko', methods=['GET'])
+def regko():
+    return render_template('regko.html')
+
+@api.route('/registrati', methods=['GET'])
+def registra():
+    return render_template('reg_ok.html')
+
 
 api.run(host="127.0.0.1", port=8085)
