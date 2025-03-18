971a1eae372b71a252e8a855bd01d2632cfc75ad
Nio4124 <antoniozottola41204@gmail.com>
2025-03-18 12:47:17 +0100

    Esercizi 3 parte 1

--- JS/P20250314/esercizio3_1.js
+++ JS/P20250314/esercizio3_1.js
@@ -1,3 +1,16 @@
-let nome = "Mario";
+// Dichiarazione e valorizzazione dei numeri
+const num1 = 10;
+const num2 = 20;
+const num3 = 30;
+const num4 = 40;
+const num5 = 50;
 
-console.log("Ciao " + nome);
+// Calcolo della somma
+const somma = num1 + num2 + num3 + num4 + num5;
+
+// Calcolo della media
+const media = somma / 5;
+
+// Output dei risultati
+console.log("Somma:", somma);
+console.log("Media:", media);
--- JS/P20250314/esercizio3_2.js
+++ JS/P20250314/esercizio3_2.js
@@ -1,3 +1,13 @@
-let nome = "Mario";
+// Dichiarazione e valorizzazione delle variabili
+const annoCorrente = new Date().getFullYear();
+const annoNascita = 1990; // Sostituisci con il tuo anno di nascita
 
-console.log("Ciao " + nome);
+// Calcolo dell'età
+const eta = annoCorrente - annoNascita;
+
+// Calcolo degli anni necessari per raggiungere i 100 anni
+const anniMancanti = 100 - eta;
+
+// Output dei risultati
+console.log("Età della persona: " + eta + " anni");
+console.log("Anni necessari per raggiungere i 100 anni: " + anniMancanti + " anni");
