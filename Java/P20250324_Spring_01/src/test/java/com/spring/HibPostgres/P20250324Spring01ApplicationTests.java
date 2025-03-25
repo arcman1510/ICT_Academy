0676cbcb69445c18a09031018b9517dc9278c1fa
Nio4124 <antoniozottola41204@gmail.com>
2025-03-25 18:09:17 +0100

    Aggiunte

---
+++ Java/P20250324_Spring_01/src/test/java/com/spring/HibPostgres/P20250324Spring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.HibPostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P20250324Spring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
