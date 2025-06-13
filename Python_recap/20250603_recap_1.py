91412ba7c8bcd1dfc199478685418c4cc944c3ff
arcman1510 <arcman1510@gmail.com>
2025-06-13 20:23:22 +0200

    r1

--- Python_recap/20250603_recap_1.py
+++ Python_recap/20250603_recap_1.py
@@ -84,7 +84,11 @@ prodotti_input: dict = {
 
 def filtra_prodotti(prodotti: dict[str, float]) -> dict[str, float]:
     nuovo_dizionario: dict = {}
-    
+    """
+  
+    """
+      
+       
     for prodotto in prodotti:
         prezzo = prodotti[prodotto]
         
@@ -97,3 +101,116 @@ def filtra_prodotti(prodotti: dict[str, float]) -> dict[str, float]:
 
 prodotti_filtrati: dict = filtra_prodotti(prodotti=prodotti_input)
 print(prodotti_filtrati)
+
+
+#Funzioni
+#1) Scrivi una funzione che verifi ca se una combinazione di condizioni (X, Y, e Z)
+# è soddisfatta per procedere con un'azione. 
+# L'azione può procedere solo se la condizione X è vera 
+# e almeno una tra Y e Z è vera. 
+# La funzione deve ritornare "Azione permessa" oppure "Azione negata" 
+# a seconda delle condizioni che sono soddisfatte.
+
+
+def condizione(X: bool, Y: bool, Z: bool):
+
+    if X == True and (Y == True or Z == True):
+        
+        return "Azione permessa"
+    
+    else:
+        
+        return "Azione negata"
+    
+    
+X: bool = True
+Y: bool = False
+Z: bool = False
+
+print(condizione(X,Y,Z))
+
+
+#Ci torna ci torna.... ci ritorna "Azione permessa" (battutina delle 10 di mattina)
+
+def condizione(numero: int) -> str:
+    
+    
+    valore_1: int = 10
+    valore_2: int = 15
+    valore_3: int = 20
+    
+    if (numero == valore_1 or numero == valore_2) and numero < valore_3:
+        
+        return "Azione permessa"
+    
+    else:
+        
+        return "Azione negata"
+    
+    
+    #2) Scrivi una funzione che moltiplica tutti i numeri interi 
+    # di una lista che sono minori di un 
+    # dato valore intero defi nito threshold.
+    
+    # threshold -> soglia
+    
+lista: list[int] = [1, 2, 3, 4]
+    
+def prodotto(lista: list[int], soglia: int) -> int:
+    
+    prodotto_cumulato: int = 1
+        
+    for valore in lista:
+        
+        if valore > soglia:
+            
+            continue
+        
+        else:
+            
+            prodotto_cumulato = prodotto_cumulato * valore
+            #prodotto_cumulato *= valore
+            
+            """prodotto = 1
+            for numero in lista_numeri:
+            if numero < threshold:
+            prodotto *= numero
+            return prodotto"""
+        
+    return prodotto_cumulato    
+            
+
+
+# scrivi una funzione che calcola il fattoriale di un numero.
+# n! = n * (n-1) * (n -2) *...*1
+# 5! = 5 * 4 * 3 * 2 * 1
+
+
+"""
+lista: list[int] = [5, 4, 3, 2, 1]
+def fattoriale(num: int) ->:
+    
+    prodotto = int = 1
+    
+    for valore in lista:
+        fatt = num * valore
+        
+    return fatt
+"""
+
+def fattoriale(numero: int) -> int:
+    
+    prodotto = int = 1
+    
+    for i in range(numero):
+        
+        prodotto *= (numero - 1)
+        
+    return prodotto
+
+
+# iterazione numero 1: (5 - 0) * 1 -> 5
+# iterazione numero 2: (5 - 1) * 5 -> 5 * 4 = 20
+# iterazione numero 3: (5 - 2) * 20 -> 3 *20 * 4 = 20
+
+print(fattoriale(numero=100))
